cfa11f0bd475b7155baef4b3c5e6562782fe7f31
tim <timdeschryver@outlook.com>
2017-09-12 21:49:31 +0200

    move to tape, add nyc and config coverage on circleci

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
-import test from 'ava';
+import test from 'tape';
 import {sum} from '../src';
 
-test('sum', t => {
-	t.is(sum(4, 7), 11);
+test('sum', (assert) => {
+	assert.equal(sum(4, 7), 11);
+	assert.end();
 });
