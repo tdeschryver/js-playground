2265281e9b57fa7bfe3e2fe69afcda45e023d08a
tim <timdeschryver@outlook.com>
2017-09-14 17:35:17 +0200

    add eslint + prettier

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 import test from 'tape';
-import {sum} from '../src';
+import { sum } from '../src';
 
-test('sum', (assert) => {
-	assert.equal(sum(4, 7), 11);
-	assert.end();
+test('sum', assert => {
+  assert.equal(sum(4, 7), 11);
+  assert.end();
 });
